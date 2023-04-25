876e17c19c62c206e897847df402c68f60213ba6
rayli <rayli@LAPTOP-K395GLJO>
2023-04-25 18:59:45 +0800

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Activity_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\Users\\rayli\\source\\repos\\StreamReader.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                StreamWriter sw = new StreamWriter("C:\\Users\\rayli\\source\\repos\\StreamReader.txt");
+
+                sw.WriteLine("Hello Activity_5!!");
+                sw.WriteLine("From Activity_5");
+
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
+
